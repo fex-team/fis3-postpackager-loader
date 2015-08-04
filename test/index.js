0ec3ce933dd0dc72582976f37c9ea7f85fbf16a2
2betop <liaoxuezhi@icloud.com>
2015-08-04 15:22:34 +0800

    use devDependency for test

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@
  */
 var fs = require('fs'),
   path   = require('path');
-var fis = require('../../..');
+var fis = require('fis3');
 var _      = fis.util,
   config = fis.config;
 var expect = require('chai').expect;
